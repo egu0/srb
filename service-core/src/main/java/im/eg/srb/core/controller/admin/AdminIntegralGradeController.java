b74fe570eb67bae11f73517cb024c84a1e71d68a
EGU0 <egu0@qq.com>
2024-11-23 16:35:27 +0800

    积分等级删除接口开发

--- service-core/src/main/java/im/eg/srb/core/controller/admin/AdminIntegralGradeController.java
+++ service-core/src/main/java/im/eg/srb/core/controller/admin/AdminIntegralGradeController.java
@@ -3,10 +3,7 @@ package im.eg.srb.core.controller.admin;
 
 import im.eg.srb.core.pojo.entity.IntegralGrade;
 import im.eg.srb.core.service.IntegralGradeService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -27,10 +24,24 @@ public class AdminIntegralGradeController {
     @Resource
     private IntegralGradeService integralGradeService;
 
-    // http://localhost:8110/admin/core/integralGrade/list
+    /**
+     * 查询积分等级列表
+     * <p>
+     * 尝试：curl http://localhost:8110/admin/core/integralGrade/list
+     */
     @GetMapping("/list")
     public List<IntegralGrade> listAll() {
         return integralGradeService.list();
+    }
+
+    /**
+     * 根据 id 删除指定的积分等级记录
+     * <p>
+     * 尝试：curl -X DELETE http://localhost:8110/admin/core/integralGrade/remove/1
+     */
+    @DeleteMapping("/remove/{id}")
+    public boolean removeById(@PathVariable Long id) {
+        return integralGradeService.removeById(id);
     }
 }
 
