60e18958e28db532be166cb29979778a8f6cbf13
EGU0 <egu0@qq.com>
2024-11-23 17:12:25 +0800

    Swagger2 常见注解

--- service-core/src/main/java/im/eg/srb/core/controller/admin/AdminIntegralGradeController.java
+++ service-core/src/main/java/im/eg/srb/core/controller/admin/AdminIntegralGradeController.java
@@ -3,6 +3,9 @@ package im.eg.srb.core.controller.admin;
 
 import im.eg.srb.core.pojo.entity.IntegralGrade;
 import im.eg.srb.core.service.IntegralGradeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -16,6 +19,7 @@ import java.util.List;
  * @author EGU0
  * @since 2024-11-23
  */
+@Api(tags = "积分等级管理")
 @CrossOrigin
 @RestController
 @RequestMapping("/admin/core/integralGrade")
@@ -29,6 +33,7 @@ public class AdminIntegralGradeController {
      * <p>
      * 尝试：curl http://localhost:8110/admin/core/integralGrade/list
      */
+    @ApiOperation("积分等级列表")
     @GetMapping("/list")
     public List<IntegralGrade> listAll() {
         return integralGradeService.list();
@@ -39,8 +44,9 @@ public class AdminIntegralGradeController {
      * <p>
      * 尝试：curl -X DELETE http://localhost:8110/admin/core/integralGrade/remove/1
      */
+    @ApiOperation(value = "根据 ID 删除积分等级", notes = "逻辑删除数据")
     @DeleteMapping("/remove/{id}")
-    public boolean removeById(@PathVariable Long id) {
+    public boolean removeById(@ApiParam(value = "记录编号 ID", example = "100", required = true) @PathVariable Long id) {
         return integralGradeService.removeById(id);
     }
 }
--- service-core/src/main/java/im/eg/srb/core/pojo/entity/IntegralGrade.java
+++ service-core/src/main/java/im/eg/srb/core/pojo/entity/IntegralGrade.java
@@ -1,16 +1,17 @@
 package im.eg.srb.core.pojo.entity;
 
-import java.math.BigDecimal;
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import java.time.LocalDateTime;
-import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableField;
-import java.io.Serializable;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -22,13 +23,13 @@ import lombok.EqualsAndHashCode;
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
-@ApiModel(value="IntegralGrade对象", description="积分等级表")
+@ApiModel(value = "IntegralGrade对象", description = "积分等级表")
 public class IntegralGrade implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @ApiModelProperty(value = "编号")
-      @TableId(value = "id", type = IdType.AUTO)
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
     @ApiModelProperty(value = "积分区间开始")
@@ -40,10 +41,10 @@ public class IntegralGrade implements Serializable {
     @ApiModelProperty(value = "借款额度")
     private BigDecimal borrowAmount;
 
-    @ApiModelProperty(value = "创建时间")
+    @ApiModelProperty(value = "创建时间", example = "2024-01-01 08:00:00")
     private LocalDateTime createTime;
 
-    @ApiModelProperty(value = "更新时间")
+    @ApiModelProperty(value = "更新时间", example = "2024-01-01 08:00:00")
     private LocalDateTime updateTime;
 
     @ApiModelProperty(value = "逻辑删除(1:已删除，0:未删除)")
