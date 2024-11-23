030ba92847e127d3ad160d9453d25ec031b0f36e
EGU0 <egu0@qq.com>
2024-11-23 17:41:45 +0800

    Swagger2 文档详细信息

--- service-base/src/main/java/im/eg/srb/base/config/Swagger2Config.java
+++ service-base/src/main/java/im/eg/srb/base/config/Swagger2Config.java
@@ -3,7 +3,10 @@ package im.eg.srb.base.config;
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,10 +18,21 @@ public class Swagger2Config {
     public Docket adminApiConfig() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName("adminApi")
+                .apiInfo(adminApiInfo())
                 .select()
                 .paths(Predicates.and(PathSelectors.regex("/admin/.*")))
                 .build();
     }
+
+    private ApiInfo adminApiInfo() {
+        return new ApiInfoBuilder()
+                .title("尚融宝后台管理系统 API 文档")
+                .description("尚融宝后台管理系统 API 详细描述信息")
+                .version("1.1")
+                .contact(new Contact("EGU0", "http://fbi.gov", "master@fbi.gov"))
+                .build();
+    }
+
 
     @Bean
     public Docket webApiConfig() {
