6a16cf778cd79d8026682d70b526328c969dc704
EGU0 <113231746+egu0@users.noreply.github.com>
2025-02-07 15:26:47 +0800

    修正返回結果中的時間格式

---
+++ service-base/src/main/java/im/eg/srb/base/config/LocalDateTimeSerializerConfig.java
@@ -0,0 +1,28 @@
+package im.eg.srb.base.config;
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * 添加此配置，用於格式化回覆體中的 LocalDateTime 數據
+ */
+@Configuration
+public class LocalDateTimeSerializerConfig {
+    @Value("${spring.jackson.date-format:yyyy-MM-dd HH:mm:ss}")
+    private String pattern;
+
+    public LocalDateTimeSerializer localDateTimeSerializer() {
+        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> builder.serializerByType(LocalDateTime.class, localDateTimeSerializer());
+    }
+}
