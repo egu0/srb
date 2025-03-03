a21b8bdec69fdb216ac7adfa3dc6cf2c4059d342
EGU0 <113231746+egu0@users.noreply.github.com>
2025-03-03 16:04:49 +0800

    投资标的分页列表

---
+++ service-core/src/main/java/im/eg/srb/core/controller/api/UserLendController.java
@@ -0,0 +1,48 @@
+package im.eg.srb.core.controller.api;
+
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import im.eg.common.result.R;
+import im.eg.srb.core.pojo.entity.Lend;
+import im.eg.srb.core.pojo.vo.LendVO;
+import im.eg.srb.core.service.LendService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * <p>
+ * 标的准备表 前端控制器
+ * </p>
+ *
+ * @author EGU0
+ * @since 2024-11-23
+ */
+@Slf4j
+@RestController
+@Api(tags = "标的")
+@RequestMapping("/api/core/lend")
+public class UserLendController {
+
+    @Resource
+    private LendService lendService;
+
+    @ApiOperation("标的分页列表")
+    @GetMapping("/list/{page}/{limit}")
+    public R listPage(@ApiParam(value = "当前页码", required = true)
+                      @PathVariable Long page,
+                      @ApiParam(value = "每页记录数", required = true)
+                      @PathVariable Long limit) {
+        Page<Lend> pageParam = new Page<>(page, limit);
+        IPage<LendVO> listModel = lendService.listPage(pageParam);
+        return R.ok().data("list", listModel);
+    }
+}
